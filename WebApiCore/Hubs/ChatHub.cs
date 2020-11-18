4ecb253650a4a40a0a3756af217fdd21196c8ff5
yisroel <weisbeym@mail.uc.edu>
2020-11-18 00:52:40 -0500

    Added group functions to chathub.

--- WebApiCore/Hubs/ChatHub.cs
+++ WebApiCore/Hubs/ChatHub.cs
@@ -13,5 +13,29 @@ namespace WebApiCore.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task SendMessageToGroup(string groupName, string message)
+        {
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId}: {message}");
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+        public async Task SendPrivateMessage(string user, string message)
+        {
+            await Clients.User(user).SendAsync("ReceiveMessage", message);
+        }
     }
 }
